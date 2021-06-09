ce125abd9e5b702957c6f04a27f2739b6490427a
Dusan Mesi <mes7456@zelsd.rs>
2021-06-09 10:39:46 +0200

    first commit

---
+++ objekti.ts
@@ -0,0 +1,22 @@
+const ZaposleniObjekt: object = {
+    ID: "1",
+    AD: "mes7456 ",
+    Maticni: "48275",
+    Prezime: "Mesi",
+    Ime: "Dusan",
+    ID_Celine: "1",
+    Hobi:['Nista1','Nista2']
+};
+
+
+
+console.log(ZaposleniObjekt)
+
+// ZaposleniObjekt = new Object();
+
+// ZaposleniObjekt.ID = $("#ID").val();
+// ZaposleniObjekt.AD = $("#AD").val();
+// ZaposleniObjekt.Maticni = $("#Maticni").val();
+// ZaposleniObjekt.Prezime = $("#Prezime").val();
+// ZaposleniObjekt.Ime = $("#Ime").val();
+// ZaposleniObjekt.ID_Celine = $("#ID_Celine").val();
---
+++ prviTS.js
@@ -0,0 +1,16 @@
+// const button = document.querySelector("button");
+// const input1 = document.getElementById("num1")! as HTMLInputElement;
+// const input2 = document.getElementById("num2")! as HTMLInputElement;
+// function add(num1: number, num2: number){
+// return num1 + num2;
+// }
+// button.addEventListener("click", function(){
+// console.log(add(+input1.value, +input2.value))
+// });
+function add(n1, n2) {
+    return n1 + n2;
+}
+var num1 = 5;
+var num2 = 2.5;
+var rezultat = add(num1, num2);
+console.log(rezultat);
